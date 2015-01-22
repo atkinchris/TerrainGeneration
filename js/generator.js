5a11c212992f1112d819e82046789aa26a33a79c
Chris Atkin <atkinchris@gmail.com>
2015-01-22 22:49:11 +0000

    Added tiles and tile rendering

--- js/generator.js
+++ js/generator.js
@@ -2,7 +2,7 @@ var CHUNK_SIZE = 128;
 var PARAMS = {
 	OCTAVES: 8,
 	ROUGHNESS: 4,
-	SCALE: 512,
+	SCALE: 32,
 	ROUND: 32,
 	PIXELATION: 32
 };
@@ -27,33 +27,33 @@ function setSeed( seed ) {
 	noise.seed( H.update( seed ).digest() );
 }
 
-function getValue( x, y ) {
-	x = _round( x / PARAMS.PIXELATION ) * PARAMS.PIXELATION;
-	y = _round( y / PARAMS.PIXELATION ) * PARAMS.PIXELATION;
+function getHeight( x, y ) {
+	//x = _round( x / PARAMS.PIXELATION ) * PARAMS.PIXELATION;
+	//y = _round( y / PARAMS.PIXELATION ) * PARAMS.PIXELATION;
 
 	x /= PARAMS.SCALE;
 	y /= PARAMS.SCALE;
 
-	value = 0;
+	height = 0;
 
 	for ( var i = 0; i < PARAMS.OCTAVES; i++ ) {
 		var frequency = Math.pow( 2, i );
 		var amplitude = Math.pow( PARAMS.ROUGHNESS / 10, i );
-		value += noise.perlin2( x * frequency, y * frequency ) * amplitude;
+		height += noise.perlin2( x * frequency, y * frequency ) * amplitude;
 	}
 
-	value = Math.abs( value );
-	value *= 255;
-	value = _round( value / PARAMS.ROUND ) * PARAMS.ROUND;
-	return value;
+	height = Math.abs( height );
+	height *= 255;
+	height = _round( height / PARAMS.ROUND ) * PARAMS.ROUND;
+	return height;
 }
 
 function generateChunk( cX, cY ) {
 	var chunk = [];
 	for ( var y = 0; y < CHUNK_SIZE; y++ ) {
 		for ( var x = 0; x < CHUNK_SIZE; x++ ) {
-			var value = getValue( cX * CHUNK_SIZE + x, cY * CHUNK_SIZE + y );
-			chunk.push( value );
+			var height = getHeight( cX * CHUNK_SIZE + x, cY * CHUNK_SIZE + y );
+			chunk.push( height );
 		}
 	}
 	return chunk;
--- js/renderer.js
+++ js/renderer.js
@@ -1,48 +1,69 @@
-PIXI.Texture.Draw = function( cb ) {
-	var canvas = document.createElement( 'canvas' );
-	if ( typeof cb == 'function' ) cb( canvas );
-	return PIXI.Texture.fromCanvas( canvas );
-}
+function main() {
+	var width = document.body.offsetWidth;
+	var height = document.body.offsetHeight;
+	var stage = new PIXI.Stage( 0x66FF99 );
+	var renderer = new PIXI.autoDetectRenderer( width, height );
+	document.body.appendChild( renderer.view );
 
-function getColour( value ) {
-	var colour = {};
-	colour.r = colour.g = colour.b = value.toString( 16 );
-	colour.a = 255;
-	return colour;
-}
+	requestAnimFrame( animate );
 
-function renderChunk( x, y ) {
-	var chunk = getChunk( x, y );
-	var chunkSize = Math.sqrt( chunk.length );
+	var assetsToLoader = [ "img/tileset.json" ];
+	loader = new PIXI.AssetLoader( assetsToLoader );
+	loader.onComplete = renderSomeChunks( width, height, stage );
+	loader.load();
 
-	var getCanvas = function( canvas ) {
-		canvas.width = chunkSize;
-		canvas.height = chunkSize;
+	function renderSomeChunks() {
+		setSeed( "Chris" );
 
-		var ctx = canvas.getContext( '2d' );
+		var chunksToRenderX = Math.ceil( width / ( CHUNK_SIZE * 16 ) );
+		var chunksToRenderY = Math.ceil( height / ( CHUNK_SIZE * 16 ) );
 
-		ctx.fillStyle = "black";
-		ctx.fillRect( 0, 0, canvas.width, canvas.height );
+		for ( var x = 0; x < chunksToRenderX; x++ ) {
+			for ( var y = 0; y < chunksToRenderY; y++ ) {
+				var chunk = renderChunk( x, y );
+				stage.addChild( chunk );
+			}
+		}
+	}
 
-		var pix = ctx.createImageData( canvas.width, canvas.height );
+	function renderChunk( cX, cY ) {
+		var tileSize = 16;
 
-		var length = chunk.length;
-		var inc = 0;
-		for ( var p = 0; p < length; p++ ) {
-			var colourRGBA = getColour( chunk[ p ] );
-			pix.data[ inc++ ] = colourRGBA.r;
-			pix.data[ inc++ ] = colourRGBA.g;
-			pix.data[ inc++ ] = colourRGBA.b;
-			pix.data[ inc++ ] = colourRGBA.a;
+		var chunk = getChunk( cX, cY );
+		var chunkSize = Math.sqrt( chunk.length );
+
+		var chunkContainer = new PIXI.DisplayObjectContainer();
+		chunkContainer.position.x = cX * chunkSize;
+		chunkContainer.position.y = cY * chunkSize;
+
+		for ( var y = 0; y < chunkSize; y++ ) {
+			for ( var x = 0; x < chunkSize; x++ ) {
+				var height = chunk[ y * chunkSize + x ];
+				if ( height <= 32 ) {
+					var tile = PIXI.Sprite.fromImage( "img/tiles/water.png" );
+				} else if ( height <= 64 ) {
+					var tile = PIXI.Sprite.fromImage( "img/tiles/grass-light.png" );
+				} else if ( height <= 96 ) {
+					var tile = PIXI.Sprite.fromImage( "img/tiles/grass.png" );
+				} else if ( height <= 128 ) {
+					var tile = PIXI.Sprite.fromImage( "img/tiles/grass-med.png" );
+				} else {
+					var tile = PIXI.Sprite.fromImage( "img/tiles/grass-dark.png" );
+				}
+
+				tile.position.x = x * tileSize;
+				tile.position.y = y * tileSize;
+				chunkContainer.addChild( tile );
+			}
 		}
 
-		ctx.putImageData( pix, 0, 0 );
-	};
+		return chunkContainer;
+	}
 
-	var sprite = new PIXI.Sprite( PIXI.Texture.Draw( getCanvas ) );
+	function animate() {
+		requestAnimFrame( animate );
 
-	sprite.position.x = x * chunkSize;
-	sprite.position.y = y * chunkSize;
-
-	return sprite;
+		// render the stage
+		renderer.render( stage );
+	}
 }
