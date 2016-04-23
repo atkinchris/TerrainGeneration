f08f693d8d780a01865152c4eefac11438a3742a
Chris Atkin <atkinchris@gmail.com>
2016-04-23 09:48:12 +0100

    Add octaves and roughness
    This adds octaves and roughness to the generation algorithm.

--- src/app.js
+++ src/app.js
@@ -5,6 +5,10 @@ const canvas = document.getElementById('container');
 const { height, width } = canvas;
 const context = canvas.getContext('2d');
 const imageData = context.createImageData(width, height);
+const params = {
+  OCTAVES: 5,
+  ROUGHNESS: 7
+};
 
 function setPixel(imageData, x, y, r, g, b, a) {
   const index = (x + y * imageData.width) * 4;
@@ -18,7 +22,15 @@ for (let y = 0; y < height; ++y) {
   for (let x = 0; x < width; ++x) {
     const cX = x / width;
     const cY = y / height;
-    const value = (noise.perlin2(cX, cY) * 128) + 128;
+    let h = 0;
+
+    for ( var i = 0; i < params.OCTAVES; i++ ) {
+      var frequency = Math.pow( 2, i );
+      var amplitude = Math.pow( params.ROUGHNESS / 10, i );
+      h += noise.perlin2( cX * frequency, cY * frequency ) * amplitude;
+    }
+
+    const value = (h * 128) + 128;
     setPixel(imageData, x, y, value, value, value, 255);
   }
 }
